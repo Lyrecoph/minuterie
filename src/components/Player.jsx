f5941f35b0640b73f11030e92ee458fec6be443b
Lyrecoph <lyrecoph30@gmail.com>
2024-08-09 00:23:07 +0100

    using useRef to simply read the value of an input field

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [enterPlayerName, setEnterPlayerName] = useState(null);
+  const playerName = useRef();
+
+  const handleClick = () => {
+    setEnterPlayerName(playerName.current.value);
+    playerName.current.value= '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { enterPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input 
+          ref={playerName} 
+          type="text"   
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
